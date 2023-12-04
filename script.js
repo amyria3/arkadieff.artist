e7b309cddece787d09f5415ab22eecb3b74539da
amyria3 <54778894+amyria3@users.noreply.github.com>
2023-12-04 22:02:13 +0100

    -

--- script.js
+++ script.js
@@ -37,83 +37,6 @@ window.addEventListener("scroll", function () {
   });
 });
 
-// script.js
-
-// 1. STABLE SLOGAN POSITION DESPITE SCROLL
-// 2. RESIZE VISIBLE VIDEO CONTENT DEPENDING ON CLICK OR SCROLL
-
-window.addEventListener("scroll", function () {
-  const sloganElement = document.getElementById("slogan");
-  sloganElement.style.transform = `translateY(${window.scrollY}px)`;
-
-  // Get the #video-background-wrapper element
-  const videoWrapper = document.getElementById("video-background-wrapper");
-
-  // Get the #disciplines section
-  const disciplinesSection = document.getElementById("disciplines");
-  const disciplinesPosition = disciplinesSection.getBoundingClientRect();
-
-  // Calculate the scroll progress based on screen height
-  const screenHeight = window.innerHeight;
-  const scrollProgress =
-    (window.scrollY - disciplinesPosition.top) / (screenHeight / 6);
-  var videoWrapperIsSmall = false;
-
-  // Shrink the video background when the scroll progress is reached
-  if (scrollProgress >= 1) {
-    const newHeight = 40; // 10vw in height
-    videoWrapper.style.height = `${newHeight}vw`;
-    videoWrapperIsSmall = true;
-  }
-
-  // Restore the original size on click if the videoWrapper is small
-  videoWrapper.addEventListener("click", function () {
-    if (videoWrapperIsSmall) {
-      videoWrapper.style.height = "100vh";
-      videoWrapper.style.maxHeight = "100vw";
-      videoWrapperIsSmall = false; // Update the state
-    }
-  });
-});
-
-// script.js
-
-// 1. STABLE SLOGAN POSITION DESPITE SCROLL
-// 2. RESIZE VISIBLE VIDEO CONTENT DEPENDING ON CLICK OR SCROLL
-
-window.addEventListener("scroll", function () {
-  const sloganElement = document.getElementById("slogan");
-  sloganElement.style.transform = `translateY(${window.scrollY}px)`;
-
-  // Get the #video-background-wrapper element
-  const videoWrapper = document.getElementById("video-background-wrapper");
-
-  // Get the #disciplines section
-  const disciplinesSection = document.getElementById("disciplines");
-  const disciplinesPosition = disciplinesSection.getBoundingClientRect();
-
-  // Calculate the scroll progress based on screen height
-  const screenHeight = window.innerHeight;
-  const scrollProgress =
-    (window.scrollY - disciplinesPosition.top) / (screenHeight / 6);
-  var videoWrapperIsSmall = false;
-
-  // Shrink the video background when the scroll progress is reached
-  if (scrollProgress >= 1) {
-    const newHeight = 40; // 10vw in height
-    videoWrapper.style.height = `${newHeight}vw`;
-    videoWrapperIsSmall = true;
-  }
-
-  // Restore the original size on click if the videoWrapper is small
-  videoWrapper.addEventListener("click", function () {
-    if (videoWrapperIsSmall) {
-      videoWrapper.style.height = "100vh";
-      videoWrapper.style.maxHeight = "100vw";
-      videoWrapperIsSmall = false; // Update the state
-    }
-  });
-});
 
 document.addEventListener("DOMContentLoaded", function () {
   // Get all elements with the class 'discipline-section'
@@ -145,25 +68,25 @@ document.addEventListener("DOMContentLoaded", function () {
     openSection(section)
   }
 
+  function openSection(sectionToBeOpened) {
+    sectionToBeOpened.classList.remove("closed");
+    const sectionContent = sectionToBeOpened.querySelector(".section-content");
+
+    sectionContent.style.width = "100%";
+    sectionToBeOpened.style.flexGrow = 1;
+    sectionContent.style.display = "flex";
+    sectionToBeOpened.classList.add("opened");
+  }
+
   function closeSection(sectionToBeClosed) {
     const sectionContent = sectionToBeClosed.querySelector(".section-content");
     sectionToBeClosed.style.flexGrow = 0;
-    sectionContent.style.width = "0";
+    sectionContent.style.width = "0%";
     setTimeout(() => {
       sectionContent.style.display = "none";
+      sectionToBeClosed.classList.add("closed");
     }, 1000);
-    sectionToBeClosed.classList.add("closed");
     sectionToBeClosed.classList.remove("opened");
-
-  }
-
-  function openSection(sectionToBeOpened) {
-    const sectionContent = sectionToBeOpened.querySelector(".section-content");
-    sectionToBeOpened.style.flexGrow = 1;
-    sectionContent.style.display = "flex";
-    sectionContent.style.width = "100%";
-    sectionToBeOpened.classList.add("opened");
-    sectionToBeOpened.classList.remove("closed");
   }
 
   // Set event listeners for mouse enter and mouse leave for every section
