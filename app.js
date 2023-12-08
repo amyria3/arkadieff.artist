4d5bcb8be63254e96b7c93e5f19296f433ad217e
amyria3 <54778894+amyria3@users.noreply.github.com>
2023-12-08 17:43:49 +0100

    -

--- app.js
+++
@@ -1,5 +0,0 @@
-import poetrySection from './Components/poetrySection.js';
-
-console.log("Vor poetrySection Aufruf");
-document.body.append(poetrySection());
-console.log("Nach poetrySection Aufruf");
