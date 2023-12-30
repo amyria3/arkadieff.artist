66304c4393e8c1be00fb653a8be306580d3a21d4
amyria3 <54778894+amyria3@users.noreply.github.com>
2023-12-31 00:00:14 +0100

    better scroll behavior thanks to absolute

--- script.js
+++ script.js
@@ -1,117 +1,134 @@
 window.addEventListener("scroll", function () {
   const scrolledVertically = window.scrollY;
-  // Initialize a variable to track the scroll direction (for SPLASH)
   let lastScrollTop = window.pageYOffset || document.documentElement.scrollTop;
   let isScrollingDown = false;
   let videoWrapperIsSmall = false;
 
-  // SPLASH
-  // Fix slogan's position
-  const sloganElement = document.getElementById("slogan");
-  if (sloganElement) {
-    sloganElement.style.transform = `translateY(${scrolledVertically / 2}px)`; // Adjust the division factor as needed
-  } else {
-    console.log("sloganElement: " + sloganElement);
-  }
-
-  // VIDEO SECTION (WRAPPER)
-  const videoWrapper = document.getElementById("video-background-wrapper");
-  const nextSection = videoWrapper.nextElementSibling;
-
-  // Function to check if scrolling down
-  function isScrollingDownward() {
-    const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-    isScrollingDown = scrollTop > lastScrollTop;
-    lastScrollTop = scrollTop;
-  }
-
-  // Function to handle scroll events
-  function handleScroll() {
-    // Calculate the scroll progress based on screen height
-    const screenHeight = window.innerHeight;
-    const scrollProgress =
-      (window.scrollY - nextSection.getBoundingClientRect().top) /
-      (screenHeight / 6);
-
-    // Shrink the video background when the scroll progress is reached
-    if (scrollProgress >= 1) {
-      const newHeight = 40; // 10vw in height
-      videoWrapper.style.height = `${newHeight}vw`;
-
-      // Add a class to indicate the videoWrapper is small
-      if (!videoWrapperIsSmall) {
-        videoWrapper.classList.add("small-video-wrapper");
-        videoWrapperIsSmall = true;
-      }
+  function handleSplash() {
+    const sloganElement = document.getElementById("slogan");
+    if (sloganElement) {
+      sloganElement.style.transform = `translateY(${scrolledVertically / 2}px)`;
     } else {
-      // If scrolling back, restore the original size
-      videoWrapper.style.height = "100vh";
+      console.log("sloganElement not found");
+    }
+  }
 
-      // Remove the class when the videoWrapper is not small
+  function handleVideoSection() {
+    const videoWrapper = document.getElementById("video-background-wrapper");
+    const nextSection = videoWrapper.nextElementSibling;
+
+    function isScrollingDownward() {
+      const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+      isScrollingDown = scrollTop > lastScrollTop;
+      lastScrollTop = scrollTop;
+    }
+
+    function shrinkVideoBackground() {
+      const screenHeight = window.innerHeight;
+      const scrollProgress =
+        (window.scrollY - nextSection.getBoundingClientRect().top) /
+        (screenHeight / 6);
+
+      if (scrollProgress >= 1) {
+        const newHeight = 40; // 10vw in height
+        videoWrapper.style.height = `${newHeight}vw`;
+
+        if (!videoWrapperIsSmall) {
+          videoWrapper.classList.add("small-video-wrapper");
+          videoWrapperIsSmall = true;
+        }
+      } else {
+        videoWrapper.style.height = "100vh";
+
+        if (videoWrapperIsSmall) {
+          videoWrapper.classList.remove("small-video-wrapper");
+          videoWrapperIsSmall = false;
+        }
+      }
+    }
+
+    window.addEventListener("scroll", function () {
+      isScrollingDownward();
+      shrinkVideoBackground();
+    });
+
+    videoWrapper.addEventListener("click", function () {
       if (videoWrapperIsSmall) {
+        videoWrapper.style.height = "100vh";
         videoWrapper.classList.remove("small-video-wrapper");
         videoWrapperIsSmall = false;
-      }
-    }
-  }
 
-  // Listen for scroll events
-  window.addEventListener("scroll", function () {
-    isScrollingDownward();
-    handleScroll();
-  });
-
-  // Restore the original size on click if the videoWrapper is small
-  videoWrapper.addEventListener("click", function () {
-    if (videoWrapperIsSmall) {
-      videoWrapper.style.height = "100vh";
-
-      // Remove the class when the videoWrapper is not small
-      videoWrapper.classList.remove("small-video-wrapper");
-      videoWrapperIsSmall = false; // Update the state
-
-      setTimeout(function () {
-        // Scroll to the videoWrapper
+        setTimeout(function () {
+          videoWrapper.scrollIntoView({ behavior: "smooth", block: "start" });
+        }, 700);
+      } else {
         videoWrapper.scrollIntoView({ behavior: "smooth", block: "start" });
-      }, 700); // 500 milliseconds (0.5 seconds)
-    } else {
-      videoWrapper.scrollIntoView({ behavior: "smooth", block: "start" });
-    }
-  });
-
-  // ARTIST
-  // Make artist-background move slowly, make #artist-content move faster
-  const artistElement = document.getElementById("artist");
-  const greySkyBackground = document.getElementById("grey-sky-background");
-  const artistContent = document.getElementById("artist-content");
-  const disciplinesWrapper = document.getElementById("disciplines-wrapper");
-
-  if (artistElement && greySkyBackground && artistContent) {
-    const distanceFromBottom =
-      window.innerHeight - artistElement.getBoundingClientRect().bottom;
-    const distanceFromBottomInPercent =
-      (distanceFromBottom / window.innerHeight) * 100;
-
-    const newBackgroundPosition = `0% ${50 + distanceFromBottomInPercent / 3}%`; // Adjust the division factor as needed
-    greySkyBackground.style.backgroundPosition = newBackgroundPosition;
-
-    const newTextPosition = `${0 + -1.07 * distanceFromBottomInPercent}%`; // Adjust as needed
-    artistContent.style.top = newTextPosition;
-  } else {
-    console.log("artistElement: " + artistElement);
-    console.log("greySkyBackground: " + greySkyBackground);
-    console.log("artistContent : " + artistContent);
+      }
+    });
   }
+
+  function handleArtistSection() {
+    const artistElement = document.getElementById("artist");
+    const greySkyBackground = document.getElementById("grey-sky-background");
+    const artistContent = document.getElementById("artist-content");
+
+    if (artistElement && greySkyBackground && artistContent) {
+      const distanceFromBottom =
+        window.innerHeight - artistElement.getBoundingClientRect().bottom;
+      const distanceFromBottomInPercent =
+        (distanceFromBottom / window.innerHeight) * 100;
+
+      const newBackgroundPosition = `0% ${50 + distanceFromBottomInPercent / 3}%`;
+      greySkyBackground.style.backgroundPosition = newBackgroundPosition;
+
+      const newTextPosition = `${0 + -1.07 * distanceFromBottomInPercent}%`;
+      artistContent.style.top = newTextPosition;
+
+      const nextDiv = findNextDivInViewport();
+
+      if (nextDiv) {
+        const distanceFromTop = nextDiv.getBoundingClientRect().top;
+
+        if (distanceFromTop < window.innerHeight * 0.8) {
+          nextDiv.style.opacity = 1;
+          nextDiv.style.transform = "translateY(0)";
+        }
+      }
+    } else {
+      console.log("Some elements in artist section not found");
+    }
+  }
+
+  function findNextDivInViewport() {
+    const allDivs = document.querySelectorAll("div");
+    let nextDiv = null;
+
+    allDivs.forEach((div) => {
+      const distanceFromTop = div.getBoundingClientRect().top;
+
+      if (
+        !nextDiv ||
+        (distanceFromTop >= 0 &&
+          distanceFromTop < nextDiv.getBoundingClientRect().top)
+      ) {
+        nextDiv = div;
+      }
+    });
+
+    return nextDiv;
+  }
+
+  handleSplash();
+  handleVideoSection();
+  handleArtistSection();
 });
 
 // PERFORMANCE
 document.addEventListener("DOMContentLoaded", function () {
-  // Lazy load all images
   document.querySelectorAll("img").forEach(function (img) {
     img.loading = "lazy";
   });
 
-  // Lazy load all videos
   document.querySelectorAll("video source").forEach(function (source) {
     source.loading = "lazy";
   });
