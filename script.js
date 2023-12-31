3e9dbb470a387fe1cac4d3502b904b1d8016e6c7
amyria3 <54778894+amyria3@users.noreply.github.com>
2023-12-31 13:50:10 +0100

    all works but a jump

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
-const sloganElement = document.getElementById("slogan");
-const artistElement = document.getElementById("artist");
+const sloganElement = document.getElementById('slogan');
+const artistElement = document.getElementById('artist');
 const greySkyBackground = document.getElementById('grey-sky-background');
 const artistContent = document.getElementById('artist-content');
 
+// Store values:
+let backgroundTopInitialPositionInVw = 20; // Change this value as needed
+let backgroundTopInitialPositionInPx = (backgroundTopInitialPositionInVw / 100) * window.innerWidth;
+let backgroundYPosition
 
 // CREATE FUNCTIONS
 // Pin the slogan
@@ -23,14 +27,42 @@ function getTopPosition(element) {
 
 // Reposition the background before
 function moveBackground() {
-  const backgroundTop = getTopPosition(greySkyBackground);
-  greySkyBackground.style.backgroundPositionY = '20rem'; // Adjust the value as needed
+  const backgroundTopInitialPosition = getTopPosition(greySkyBackground);
+  greySkyBackground.style.backgroundPositionY = backgroundTopInitialPosition; // Adjust the value as needed
 }
 
 // Function to slowly move the background in the opposite direction as you scroll in #artist (slower than scroll)
 function slowMoveBackground() {
   const scrolledVertically = window.scrollY;
   greySkyBackground.style.backgroundPositionY = `-${scrolledVertically/2}px`; // Note the negative sign
+}
+
+// Function to get the yPosition of the background
+function updateBackgroundPosition(){
+  var computedStyle = window.getComputedStyle(greySkyBackground);
+  var currentBackgroundPositionY = computedStyle.getPropertyValue('background-position-y');
+  backgroundYPosition = currentBackgroundPositionY
+}
+
+// Function to execute on #artist-content-scroll
+function handleArtistBackground() {
+  if (!artistContent || !greySkyBackground) {
+    return;
+  }
+
+  const artistContentHeight = artistContent.scrollHeight;
+  const windowHeight = window.innerHeight;
+  const scrolledVertically = artistContent.scrollTop;
+  const maxScroll = artistContentHeight - windowHeight;
+  const scrollProgress = (maxScroll === 0) ? 0 : (scrolledVertically / maxScroll) * 100;
+
+  // Adjust the multiplier as needed for the desired effect
+  const backgroundPosition = backgroundTopInitialPositionInPx - (scrollProgress * 2); // Adjust the multiplier
+
+  // Check if backgroundPosition is a valid number and within a reasonable range
+  if (!isNaN(backgroundPosition) && isFinite(backgroundPosition) && Math.abs(backgroundPosition) < 1000000) {
+    greySkyBackground.style.backgroundPositionY = `${backgroundPosition}px`;
+  }
 }
 
 // Summarize Functions
@@ -41,7 +73,7 @@ function handleScroll() {
 }
 
 window.addEventListener("scroll", handleScroll);
-
+artistContent.addEventListener("scroll", handleArtistBackground)
 
 // PERFORMANCE
 document.addEventListener("DOMContentLoaded", function () {
