0a1b8dc2280aef139f6eb6963ffc32cad590211d
amyria3 <54778894+amyria3@users.noreply.github.com>
2024-01-01 23:34:21 +0100

    interaction (scroll to) works

--- menu.js
+++ menu.js
@@ -1,11 +1,26 @@
-  $(document).ready(function () {
-    $(".menu-item").on("click", function () {
-      var targetId = $(this).data("anchor");
-      var targetSection = $("#" + targetId);
+document.addEventListener("DOMContentLoaded", function () {
+    var menuItems = document.querySelectorAll(".menu-item");
 
-      if (targetSection.length) {
-        var offset = targetSection.offset().top;
-        $("#wrapper").animate({scrollTop: offset}, 500);
-      }
+    menuItems.forEach(function (item) {
+      item.addEventListener("click", function () {
+        // Remove 'selected-item' class from all menu items
+        menuItems.forEach(function (menuItem) {
+          menuItem.classList.remove("selected-item");
+        });
+
+        // Add 'selected-item' class to the clicked menu item
+        this.classList.add("selected-item");
+
+        var targetId = this.getAttribute("data-anchor");
+        console.log("Clicked on:", targetId);
+
+        var targetSection = document.getElementById(targetId);
+
+        if (targetSection) {
+          var offset = targetSection.offsetTop;
+          console.log("Scrolling to offset:", offset);
+          document.getElementById("artist-content").scrollTo({ top: offset, behavior: "smooth" });
+        }
+      });
     });
   });
