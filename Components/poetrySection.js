66a681e30dd4c4b18af215b04e9d212bf77d5328
amyria3 <54778894+amyria3@users.noreply.github.com>
2024-01-15 15:17:11 +0100

    -

--- Components/poetrySection.js
+++
@@ -1,9 +0,0 @@
-import poetryElement from "./poetryElement.js";
-
-export default function poetrySection() {
-  const section = document.createElement("section");
-  section.id = "poetrySection";
-
-  section.appendChild(poetryElement("This is my poetry text", "text"));
-  return section
-}
